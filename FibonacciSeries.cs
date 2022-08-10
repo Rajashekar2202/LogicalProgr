53cddc6940c3ef68b31ab2fc8bddbc1a8345ebf7
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 23:47:31 +0530

    added FibonacciSeries program

---
+++ FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    public static class FibonacciSeries
+    {
+        public static void DisplayFibonacciSeries()
+        {
+            int n = 10, firstTerm = 0, secondTerm = 1;
+            Console.WriteLine("Fabonacci series till " + n + " terms:");
+
+            for (int i = 1; i <= n; ++i)
+            {
+                Console.WriteLine(firstTerm + ",");
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+
+            }
+        }
+    }
+}
