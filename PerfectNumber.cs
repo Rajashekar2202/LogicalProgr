66e99a9489b8bf7961cd7b1fc1db9eda90b54094
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 23:51:17 +0530

    added PerfectNumber program

---
+++ PerfectNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    public static class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            Console.WriteLine("Enter the Number: ");
+            int sum = 0;
+            int i = 1;
+
+           
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= n / 2)
+            {
+
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
+                i++;
+            }
+            if (sum == n)
+            {
+                Console.WriteLine(n + " is a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine(n + " is not a Perfect Number");
+            }
+
+        }
+    }
+}
