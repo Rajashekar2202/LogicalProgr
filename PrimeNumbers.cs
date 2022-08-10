b8e718f9f3257cc90f43e44024e4245b176ddb00
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 23:54:17 +0530

    added PrimeNumber program

---
+++ PrimeNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming
+{
+    public static class PrimeNumbers
+    {
+        public static void PrimeNumber()
+        {
+            Console.WriteLine("Enter the Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 2; i <= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(num + " is not prime number");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(num + " is prime number");
+                    break;
+                }
+            }
+        }
+    }
+}
